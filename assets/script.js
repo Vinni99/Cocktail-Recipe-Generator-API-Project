95031ea221921864e49496ea6d6771b5cb01442d
Billy Massie <113217574+AltB22@users.noreply.github.com>
2022-12-08 15:48:58 -0800

    Revert "Massie branch"

--- assets/script.js
+++ assets/script.js
@@ -45,51 +45,26 @@ var fetchNameButton = document.getElementById("name-button")
 function getDrinks(event) {
   event.preventDefault()
   var drinkNameContainer = document.getElementById("drink-input").value
- //console.log(drinkNameContainer)
+  console.log(drinkNameContainer)
     var requestUrl1 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameContainer;
 // var requestUrl2 = event.target.name === "nameSearch" ? "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkNameContainer : "Other API CALL HERE"
-  // console.log(requestUrl1)
+  console.log(requestUrl1)
 
   fetch(requestUrl1)
     .then(function (response) {
+// console.log(requestUrl1)
         return response.json();
     })
 //Need to define what the for loop is looking for here or at least before the for loop itself
     .then(function (drinkData) {
-      // console.log(drinkData)
+      console.log(drinkData)
+      var createDrinkRow = document.createElement('ul');
       for(let i = 0; i < drinkData.drinks.length; i++){
-
-        var drinkContainerEl = document.createElement('div')
-        // this could also be a button element
-        var drinkTitle =document.createElement('h4');
-        var drinkButton =document.createElement('button');
-        var ingredientContainer = document.createElement('ul');
-
         console.log(drinkData.drinks[i])
-        drinkTitle.textContent = drinkData.drinks[i].strDrink;
-        drinkButton.textContent = "Save Drink"
-        
-        var drinkIngredients = [drinkData.drinks[i].strIngredient1, drinkData.drinks[i].strIngredient2, drinkData.drinks[i].strIngredient3, drinkData.drinks[i].strIngredient4, drinkData.drinks[i].strIngredient5, drinkData.drinks[i].strIngredient6, drinkData.drinks[i].strIngredient7, drinkData.drinks[i].strIngredient8,drinkData.drinks[i].strIngredient9,drinkData.drinks[i].strIngredient10,drinkData.drinks[i].strIngredient11,drinkData.drinks[i].strIngredient12,drinkData.drinks[i].strIngredient13,drinkData.drinks[i].strIngredient14,drinkData.drinks[i].strIngredient15,]
-        
-        console.log(drinkIngredients)
-        
-        var ingredientContainerTitle = document.createElement('h5')
-        ingredientContainerTitle.textContent = 'Ingredients: '
-
-        for(var j=0; j< drinkIngredients.length; j++){
-          if (drinkIngredients[j] !== null){
-            var drinkRowData = document.createElement('li')
-        
-            drinkRowData.textContent = drinkIngredients[j]
-
-          }
-          
-          ingredientContainer.append(drinkRowData)
-        }
-// comment this back in to display the button next to the drink name
-        drinkTitle.append(drinkButton)
-        drinkContainerEl.append(drinkTitle,ingredientContainerTitle, ingredientContainer)
-        searchedDrinks.append(drinkContainerEl)
+        var drinkRowData = document.createElement('li')
+        drinkRowData.textContent = drinkData.drinks[i].strDrink
+        createDrinkRow.appendChild(drinkRowData)
+        searchedDrinks.append(createDrinkRow)
         // drinkRowData.appendChild(createDrinkRow)
 
       }
